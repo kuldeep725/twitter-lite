97b6f9af829623ec5e4237724ea3135b730289f8
Kuldeep Singh Bhandari <singh.kuldeepbhandari@gmail.com>
2021-12-05 14:04:11 +0530

    Db config setup for local and cloud

---
+++ src/main/java/com/kunal/twitterlite/configuration/DbConfiguration.java
@@ -0,0 +1,42 @@
+package com.kunal.twitterlite.configuration;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+public class DbConfiguration {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        final String ENV_DATABASE_URL = System.getenv("DATABASE_URL");
+
+        if(ENV_DATABASE_URL != null) {
+            URI dbUri = new URI(ENV_DATABASE_URL);
+
+            username = dbUri.getUserInfo().split(":")[0];
+            password = dbUri.getUserInfo().split(":")[1];
+            dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+        }
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+}
